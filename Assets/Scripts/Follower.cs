8095fcc183c475227cfc9c8b94b2d011f3326914
mountain <thesailorgamer0@gmail.com>
2022-12-12 20:49:24 +0000

    Made sandbox with test environment

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using PathCreation;
+using Unity.VisualScripting;
+
+public class Follower : MonoBehaviour
+{
+    public static Follower instance = null;
+    public PathCreator leftLane;
+    public PathCreator rightLane;
+    public PathCreator middleLane;
+    float speed;
+    float distTravelled;
+    int lane = 1;
+    public PathCreator pathCreator;
+
+    private void Start()
+    {
+        Lane();
+        speed = 5;
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (lane == 1 || lane == 2)
+            {
+                lane--;
+                Lane();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (lane == 0 || lane == 1)
+            {
+                lane++;
+                Lane();
+            }
+        }
+
+        distTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distTravelled);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(distTravelled);
+    }
+
+    void Lane()
+    {
+        if (lane == 1)
+        {
+            pathCreator = middleLane;
+        } else if (lane == 2)
+        {
+            pathCreator = rightLane;
+        } else if (lane == 0)
+        {
+            pathCreator = leftLane;
+        }
+    }
+
+    public void increaseSpeed()
+    {
+        speed += 5;
+        Debug.Log("Speed increased by 5");
+    }
+}
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    public static GameState instance = null;
+    private int score;
+    private int coinsCollected;
+    public GameObject[] coins;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Start()
+    {
+        coins = GameObject.FindGameObjectsWithTag("Coin");
+    }
+
+    public void coinCollected()
+    {
+        coinsCollected++;
+        score = score + 100;
+        Debug.Log("Coins collected: " + coinsCollected);
+        Debug.Log("Score: " + score);
+    }
+
+    public void lap()
+    {
+        score = score + 1000;
+        Follower.instance.increaseSpeed();
+
+        // reload all coins
+        foreach (GameObject coin in coins)
+        {
+            coin.GetComponent<Renderer>().enabled = true;
+        }
+    }
+}
